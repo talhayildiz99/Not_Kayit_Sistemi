3976c02184fcc511272c27c81daace498ac947c5
Talha <99talhayildiz@gmail.com>
2023-06-05 15:32:59 +0300

    Add project files.

---
+++ Not_Kayit_Sistemi/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Not_Kayit_Sistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(118, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Öğrenci Numarası";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(43, 72);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(324, 30);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.Location = new System.Drawing.Point(43, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(324, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Giriş Yap";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(432, 195);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Not_Kayit_Sistemi/FrmOgrenciDetay.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Not_Kayit_Sistemi
+{
+    partial class FrmOgrenciDetay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblAdSoyad = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LblNumara = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.LblSinav1 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.LblSinav3 = new System.Windows.Forms.Label();
+            this.LblSinav2 = new System.Windows.Forms.Label();
+            this.LblOratalama = new System.Windows.Forms.Label();
+            this.LblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(32, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad:";
+            // 
+            // LblAdSoyad
+            // 
+            this.LblAdSoyad.AutoSize = true;
+            this.LblAdSoyad.ForeColor = System.Drawing.Color.White;
+            this.LblAdSoyad.Location = new System.Drawing.Point(147, 37);
+            this.LblAdSoyad.Name = "LblAdSoyad";
+            this.LblAdSoyad.Size = new System.Drawing.Size(64, 24);
+            this.LblAdSoyad.TabIndex = 1;
+            this.LblAdSoyad.Text = "NULL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(32, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numara:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(32, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sınav 1:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(32, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ortalama:";
+            // 
+            // LblNumara
+            // 
+            this.LblNumara.AutoSize = true;
+            this.LblNumara.ForeColor = System.Drawing.Color.White;
+            this.LblNumara.Location = new System.Drawing.Point(147, 82);
+            this.LblNumara.Name = "LblNumara";
+            this.LblNumara.Size = new System.Drawing.Size(58, 24);
+            this.LblNumara.TabIndex = 6;
+            this.LblNumara.Text = "0000";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(32, 317);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 24);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Durum:";
+            // 
+            // LblSinav1
+            // 
+            this.LblSinav1.AutoSize = true;
+            this.LblSinav1.ForeColor = System.Drawing.Color.White;
+            this.LblSinav1.Location = new System.Drawing.Point(147, 128);
+            this.LblSinav1.Name = "LblSinav1";
+            this.LblSinav1.Size = new System.Drawing.Size(34, 24);
+            this.LblSinav1.TabIndex = 9;
+            this.LblSinav1.Text = "00";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(32, 218);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(81, 24);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Sınav 3:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(32, 175);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(81, 24);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Sınav 2:";
+            // 
+            // LblSinav3
+            // 
+            this.LblSinav3.AutoSize = true;
+            this.LblSinav3.ForeColor = System.Drawing.Color.White;
+            this.LblSinav3.Location = new System.Drawing.Point(147, 218);
+            this.LblSinav3.Name = "LblSinav3";
+            this.LblSinav3.Size = new System.Drawing.Size(34, 24);
+            this.LblSinav3.TabIndex = 12;
+            this.LblSinav3.Text = "00";
+            // 
+            // LblSinav2
+            // 
+            this.LblSinav2.AutoSize = true;
+            this.LblSinav2.ForeColor = System.Drawing.Color.White;
+            this.LblSinav2.Location = new System.Drawing.Point(147, 175);
+            this.LblSinav2.Name = "LblSinav2";
+            this.LblSinav2.Size = new System.Drawing.Size(34, 24);
+            this.LblSinav2.TabIndex = 13;
+            this.LblSinav2.Text = "00";
+            // 
+            // LblOratalama
+            // 
+            this.LblOratalama.AutoSize = true;
+            this.LblOratalama.ForeColor = System.Drawing.Color.White;
+            this.LblOratalama.Location = new System.Drawing.Point(147, 270);
+            this.LblOratalama.Name = "LblOratalama";
+            this.LblOratalama.Size = new System.Drawing.Size(34, 24);
+            this.LblOratalama.TabIndex = 14;
+            this.LblOratalama.Text = "00";
+            // 
+            // LblDurum
+            // 
+            this.LblDurum.AutoSize = true;
+            this.LblDurum.ForeColor = System.Drawing.Color.White;
+            this.LblDurum.Location = new System.Drawing.Point(147, 317);
+            this.LblDurum.Name = "LblDurum";
+            this.LblDurum.Size = new System.Drawing.Size(64, 24);
+            this.LblDurum.TabIndex = 15;
+            this.LblDurum.Text = "NULL";
+            // 
+            // FrmOgrenciDetay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(272, 376);
+            this.Controls.Add(this.LblDurum);
+            this.Controls.Add(this.LblOratalama);
+            this.Controls.Add(this.LblSinav2);
+            this.Controls.Add(this.LblSinav3);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.LblSinav1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.LblNumara);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LblAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmOgrenciDetay";
+            this.Text = "Öğrenci Not Kayıt Sistemi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblAdSoyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LblNumara;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label LblSinav1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label LblSinav3;
+        private System.Windows.Forms.Label LblSinav2;
+        private System.Windows.Forms.Label LblOratalama;
+        private System.Windows.Forms.Label LblDurum;
+    }
+}
